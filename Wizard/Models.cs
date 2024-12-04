a378924ee9b4f18198431a7dd29b2a2c381083cf
Hwiet <42421547+Hwiet@users.noreply.github.com>
2024-12-04 14:43:52 -0500

    Reproduce problem with deferred loading

---
+++ Wizard/Models.cs
@@ -0,0 +1,21 @@
+﻿namespace Wizard;
+
+public class IntroModel
+{
+
+}
+
+public class GetStartedModel
+{
+    public string? ProjectName { get; set; }
+}
+
+public class SetBudgetModel
+{
+
+}
+
+public class SummaryModel
+{
+
+}
---
+++ Wizard/Pages/Home.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+namespace Wizard.Pages;
+
+public partial class Home
+{
+    [Inject]
+    public IDialogService DialogService { get; set; } = default!;
+
+    public IntroModel IntroModel { get; set; } = new();
+
+    public GetStartedModel GetStartedModel { get; set; } = new();
+
+    public SetBudgetModel SetBudgetModel { get; set; } = new();
+
+    public SummaryModel SummaryModel { get; set; } = new();
+
+    private async Task ShowSubmitMessageAsync(string message)
+    {
+        var dialogRef = await DialogService.ShowSuccessAsync(message);
+        await dialogRef.Result;
+    }
+}
